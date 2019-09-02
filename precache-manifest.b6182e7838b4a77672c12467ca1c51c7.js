eef7634628e3fda8d61c3f0681f6a7563f1411d4
Santiago Hernández <santiagohernandez.1997@gmail.com>
2019-09-01 21:41:43 -0300

    Updates

---
+++ precache-manifest.b6182e7838b4a77672c12467ca1c51c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20eb09fa56184a448fbeaca37f0ea62c",
+    "url": "/CCK-demo/index.html"
+  },
+  {
+    "revision": "b14c56d06380c58478e4",
+    "url": "/CCK-demo/static/css/main.e090879b.chunk.css"
+  },
+  {
+    "revision": "3ce93e94390071cb94a7",
+    "url": "/CCK-demo/static/js/2.270be295.chunk.js"
+  },
+  {
+    "revision": "b14c56d06380c58478e4",
+    "url": "/CCK-demo/static/js/main.9fc4c7d4.chunk.js"
+  },
+  {
+    "revision": "70518dd45a3f24129954",
+    "url": "/CCK-demo/static/js/runtime~main.c3ae1c9c.js"
+  }
+]);
